6fc6980d179ef3bf97d563e275b3e44d637be0ed
wilgrzelak <williamgrzelak@gmail.com>
2019-10-09 19:26:55 -0400

    Started movie list project

---
+++ MovieList/MovieList/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieList
+{
+   public class Movie
+    {
+        public string Title;
+        public int ReleaseYear;
+        public Movie(string title, int releaseYear)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+        }
+        public void ShowDetails()
+        {
+            MessageBox.Show(Title + "was released in" + ReleaseYear);
+        }
+
+    }
+}
